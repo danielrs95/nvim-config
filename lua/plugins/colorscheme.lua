35db535f55e5b9965c849d3fa543bde0b4c62ef0
Daniel <danielrs9504@gmail.com>
2023-10-30 08:33:16 -0500

    colorscheme
    change colorscheme from nightffly to catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,23 +1,25 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = true,
-  --   opts = { style = "storm" },
-  -- },
   {
-    "bluz71/vim-nightfly-guicolors",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
-    end,
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "storm" },
   },
+  -- {
+  --   "bluz71/vim-nightfly-guicolors",
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     -- load the colorscheme here
+  --     vim.cmd([[colorscheme nightfly]])
+  --   end,
+  -- },
 
-  -- Configure LazyVim to load gruvbox
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim to load catpuccinn
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfly",
+      colorscheme = "catppuccin",
     },
   },
 }
