6f3eca85f2ddaabf1e21bfa58d2299f388adaf3a
Daniel <danielrs9504@gmail.com>
2024-01-29 17:03:34 -0500

    backup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,22 +4,23 @@ return {
     lazy = true,
     opts = { style = "storm" },
   },
-  -- {
-  --   "bluz71/vim-nightfly-guicolors",
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     -- load the colorscheme here
-  --     vim.cmd([[colorscheme nightfly]])
-  --   end,
-  -- },
+  {
+    "bluz71/vim-nightfly-guicolors",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme nightfly]])
+    end,
+  },
 
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Configure LazyVim to load catpuccinn
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
 }
